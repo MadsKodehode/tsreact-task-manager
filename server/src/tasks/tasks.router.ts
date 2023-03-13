322ffe8c2b879601df9ddc81e1bae928b17b8228
MadsKodehode <mads.kodehode@gmail.com>
2023-03-13 20:05:26 +0100

    Added put method for task controller

--- server/src/tasks/tasks.router.ts
+++ server/src/tasks/tasks.router.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import { taskController } from './tasks.controller';
 import { createValidator } from './tasks.validator';
+import { updateValidator } from './tasks.validator';
 
 export const taskRouter: Router = Router();
 
 taskRouter.get('/', taskController.getAll);
 
 taskRouter.post('/', createValidator, taskController.create);
+taskRouter.put('/', updateValidator, taskController.update);
